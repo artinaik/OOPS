3d3bdd6e4a5d94d628ef79882ba1a9b0b6d4e44f
Your UserName <aratinaik151997@gmail.com>
2021-11-18 14:34:50 +0530

    OOPSConcepts project added having Inheritance Feature

---
+++ OOPSConcepts/OOPSConcepts/Inheritance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcepts
+{
+    class Inheritance
+    {
+       public double mathMarks =55.5;
+       public double englishMarks=60;
+       public double hindiMarks = 64.5;
+       public double scienceMarks =70;
+        public double totalMarks;
+        public double Total()
+        {
+            totalMarks = mathMarks + englishMarks + hindiMarks + scienceMarks;
+            return totalMarks;
+        }
+    }
+    class AvarageMarks:Inheritance
+    {
+        public double avg;
+        public void FindAvg()
+        {
+            Total();
+            avg = totalMarks / 4;
+            Console.WriteLine("Avarage marks is " + avg);
+        }
+    }
+}
---
+++ OOPSConcepts/OOPSConcepts/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OOPS Concepts");
+            Console.WriteLine("1)Inheritance");
+            AvarageMarks avarageMarksobj = new AvarageMarks();
+            avarageMarksobj.FindAvg();
+            Console.ReadKey();
+
+        }
+    }
+}
